85d99aa6a78ff42fb7c8a0fbbb75dfd184ad550a
benjamin gutierrez <benjamingutiterrez89@gmail.com>
2024-01-26 14:27:51 +0100

    haciendo que sume los numero

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 /* inicializando el juego*/
 initApp()
 /*declarando variables */
-
+let cuenta = 0
 let cara
 const btnroll = document.querySelector('.btn--roll')
 const dado = document.querySelector('.dice')
+let current1 = document.getElementById('current--0')
 /*funcion para inciar el juego*/
 function initApp() {
   let score = 0
@@ -18,10 +19,13 @@ function initApp() {
 
 btnroll.addEventListener('click', () => {
   cara = Math.floor(Math.random() * 6 + 1)
+  cuenta += cara
+  current1.textContent = cuenta
 
   switch (cara) {
     case 1:
       dado.setAttribute('src', 'dice-1.png')
+      current1.textContent = 0
       break
     case 2:
       dado.setAttribute('src', 'dice-2.png')
